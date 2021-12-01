75452a19826edc3e7c815ffb4443f9857454b5c1
Eunji Nam <godangel31@gmail.com>
2021-12-01 18:27:16 +0900

    mall/Shop에 tel과 addr 필드 추가

--- myhomework13/mall/models.py
+++ myhomework13/mall/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Shop(models.Model):
     name = models.CharField(max_length=100)
+    tel = models.CharField(max_length=15)
+    addr = models.CharField(blank=True, max_length=20)
     description = models.TextField(blank=True)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
