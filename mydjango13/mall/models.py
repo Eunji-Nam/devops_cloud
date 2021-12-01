13df53dc078916b4c0f2bdf12dc5ffd4b4c80840
Eunji Nam <godangel31@gmail.com>
2021-12-01 17:00:37 +0900

    Django Model 연습 노트북 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -7,9 +7,13 @@ class Shop(models.Model):
     # -- 장고에서는 id를 데이터에 대한 address로 여겨서 자동으로 생기기 떄문에 굳이 설정할 필요 없음
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    telephone = models.CharField(max_length=13, )
+    telephone = models.CharField(max_length=13)
     # open_time = models.TimeField(default=datetime.time)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        # 관련 쿼리셋에서 order_by를 지정하지 않을 때 
+        # 적용될 디폴트 정렬
+        ordering = ['-id']
