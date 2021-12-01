da26eff959478d690a332c12e281120aef9b6fde
Eunji Nam <godangel31@gmail.com>
2021-12-01 13:16:28 +0900

    models에 telephone 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -1,10 +1,13 @@
+import datetime
 from django.db import models
 
 
 class Shop(models.Model):
-    # id = models.BigAutoField(primary_key=True)
+    # id = models.BigAutoField(primary_key=True)  -- 굳이 설정할 필요 없음
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13, )
+    # open_time = models.TimeField(default=datetime.time)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
