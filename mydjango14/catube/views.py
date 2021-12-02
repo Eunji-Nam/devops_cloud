0dd3f15ebb2768cf669e3129d4476719b98f5306
Eunji Nam <godangel31@gmail.com>
2021-12-02 15:44:21 +0900

    Video 영상 재생 구현

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "catube/index.html")
+from catube.models import Video
 
+
+# Class Based View(CBV)
+index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+
+# Function Based View(FBV)
+
+# def index(request: HttpRequest) -> HttpResponse:
+#     qs = Video.objects.all()
+#     return render(request, "catube/index.html", {
+#         "video_list": qs,
+#     })
+
+def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(request, "catube/video_detail.html",
+        {
+            "video": video,
+        },
+    )
