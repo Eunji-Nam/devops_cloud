c7c3434770d2932269c3f62f7f5bc4c2c611e7fa
Eunji Nam <godangel31@gmail.com>
2021-12-06 01:58:44 +0900

    MEDIA 세팅

--- myhomework15/myhomework15/urls.py
+++ myhomework15/myhomework15/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,3 +13,6 @@ if settings.DEBUG:
         path('__debug__/',
              include(debug_toolbar.urls))
     ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
