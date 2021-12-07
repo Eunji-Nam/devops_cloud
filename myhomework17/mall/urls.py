3ecbc1facbc4ab9b05d276fe4b24ed83bffc127a
Eunji Nam <godangel31@gmail.com>
2021-12-08 05:07:14 +0900

    cloth_detail 뷰 구현

--- myhomework17/mall/urls.py
+++ myhomework17/mall/urls.py
@@ -5,4 +5,5 @@ app_name = 'mall'
 
 urlpatterns = [
     path('', views.cloth_list, name="cloth_list"),
+    path('<int:pk>/', views.cloth_detail, name="cloth_detail"),
 ]
--- myhomework17/mall/views.py
+++ myhomework17/mall/views.py
@@ -6,4 +6,10 @@ from mall.models import Cloth
 def cloth_list(request: HttpRequest) -> HttpResponse:
     qs = Cloth.objects.all()
     qs = qs.order_by("-pk")
-    return render(request, "mall/cloth_list.html", {})
+    return render(request, "mall/cloth_list.html", {
+        "cloth_list":qs,
+    })
+
+def cloth_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    cloth = Cloth.objects.get(pk=pk)
+    return render(request, "mall/cloth_detail.html", {})
