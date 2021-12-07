d0c5253e765a0fac7de21c879c8467d5039a774f
Eunji Nam <godangel31@gmail.com>
2021-12-08 04:47:37 +0900

    cloth_list 뷰 구현

--- myhomework17/mall/urls.py
+++ myhomework17/mall/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
 from mall import views
 
-urlpatterns = []
+app_name = 'mall'
+
+urlpatterns = [
+    path('', views.cloth_list, name="cloth_list"),
+]
--- myhomework17/mall/views.py
+++ myhomework17/mall/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from mall.models import Cloth
 
-# Create your views here.
+
+def cloth_list(request: HttpRequest) -> HttpResponse:
+    qs = Cloth.objects.all()
+    qs = qs.order_by("-pk")
+    return render(request, "mall/cloth_list.html", {})
