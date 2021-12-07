1b9f03c9d7627abc9ea6d6abf1c4dd89839570fd
Eunji Nam <godangel31@gmail.com>
2021-12-08 05:19:50 +0900

    cloth_list 뷰에 간단 검색 기능 추가

--- myhomework17/mall/views.py
+++ myhomework17/mall/views.py
@@ -6,6 +6,9 @@ from mall.models import Cloth
 def cloth_list(request: HttpRequest) -> HttpResponse:
     qs = Cloth.objects.all()
     qs = qs.order_by("-pk")
+    q = request.GET.get("q", "")
+    if q:
+        qs = qs.filter(name__icontains=q)
     return render(request, "mall/cloth_list.html", {
         "cloth_list":qs,
     })
