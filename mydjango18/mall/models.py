be1f3dc7ceb6de389c0446e030eb678d74654820
Eunji Nam <godangel31@gmail.com>
2021-12-08 16:15:20 +0900

    mall 앱 생성 및 마이그래이션

--- mydjango18/mall/models.py
+++ mydjango18/mall/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Cloth(TimestampedModel):
+    name = models.CharField(max_length=20, db_index=True)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=10)
+    price = models.PositiveIntegerField()
+    content = models.TextField()
+    photo = models.ImageField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+
+class Review(TimestampedModel):
+    cloth = models.ForeignKey(Cloth, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+
+class Tag(TimestampedModel):
+    category = models.CharField(max_length=10, db_index=True)
+
