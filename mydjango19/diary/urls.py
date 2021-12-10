40efca44d0c1ec08e05f9b6278d328492a01cab8
Eunji Nam <godangel31@gmail.com>
2021-12-10 14:52:57 +0900

    commet_edit 구현

--- mydjango19/diary/urls.py
+++ mydjango19/diary/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:pk>/', views.post_detail, name="post_detail"),
     path('new/', views.post_new, name="post_new"),
     path('<int:pk>/edit/', views.post_edit, name="post_edit"),
-    path('tags/<str:tag_name>/', views.tag_detail, name="tag_detail")
-
-]
+    path('tags/<str:tag_name>/', views.tag_detail, name="tag_detail"),
+    path('<int:post_pk>/comments/new/', views.comment_new, name="comment_new"),
+    path('<int:post_pk>/comments/<int:pk>/edit/', views.comment_edit, name="comment_edit"),
+    ]
--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
-from diary.forms import PostForm
-from diary.models import Post
+from diary.forms import PostForm, CommentForm
+from diary.models import Post, Comment
 
 
 def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
@@ -25,7 +25,12 @@ def post_list(request: HttpRequest) -> HttpResponse:
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+
+    # try:
+    #     post = Post.objects.get(pk=pk)   # DoesNotExist 에러
+    # except Post.DoesNotExist:
+    #     raise Http404 # 예외발생
     comment_list = post.comment_set.all()
     tag_list = post.tag_set.all()
     return render(request, "diary/post_detail.html", {
@@ -54,7 +59,7 @@ def post_new(request: HttpRequest) -> HttpResponse:
 
 def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
     # 아래 코드는 ModelForm에 한해서 동작하는 코드.
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
 
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES, instance=post)
@@ -69,3 +74,39 @@ def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
         "form": form,
     })
 
+# diary/100/comments/new/
+def comment_new(request: HttpRequest, post_pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == "POST": # 항상 대문자로!!
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            # form.cleaned_data  # 유효성 검사에 통화한 값들 (타입은 dict)
+            comment = form.save(commit=False)
+            # comment.post_id = post_pk    # FK를 직접 채우진 않음
+            comment.post = post
+            comment.save()
+            return redirect("diary:post_detail", post_pk)
+    else:
+        form = CommentForm()
+    return render(request, "diary/comment_form.html", {
+        "form": form,
+    })
+
+
+# /diary/100/comments/20/edit/
+def comment_edit(request: HttpRequest, post_pk: int, pk: int) -> HttpResponse:
+    comment = get_object_or_404(Comment, pk=pk)
+    # form = CommentForm(instance=comment)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES, instance=comment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "성공적으로 수정했습니다.")
+            return redirect("diary:post_detail", post_pk)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, "diary/comment_form.html", {
+        "form": form,
+    })
