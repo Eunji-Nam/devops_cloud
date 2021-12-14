245d2a4d0319b7da7b13ca492504224e2ac9347b
Eunji Nam <godangel31@gmail.com>
2021-12-14 10:01:07 +0900

    Category 모델 추가 및 bootstrap5 적용

--- mydjango20/shop/admin.py
+++ mydjango20/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.forms import ShopForm
+from shop.models import Shop, Review, Tag, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    form = ShopForm
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
