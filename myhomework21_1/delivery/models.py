e24cbeee010e3b435ad21f3b4474cb0ea06ebfd6
Eunji Nam <godangel31@gmail.com>
2021-12-14 14:28:53 +0900

    delivery/models 생성

--- myhomework21_1/delivery/models.py
+++ myhomework21_1/delivery/models.py
@@ -1,3 +1,55 @@
+from django.core.validators import RegexValidator
 from django.db import models
+
+
+# class TimestampedModel(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     class Mete:
+#         abstract = True
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Shop(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3.4}-?\d{3,4}-?\d{4}$",
+                                                    message="전화번호를 입력해주세요.")],
+                                 help_text="입력 예) 042-000-0000")
+    teg_set = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Review(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
