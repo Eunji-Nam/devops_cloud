80710ed2123f2207b7a8422e70bf960b80b091e1
Eunji Nam <godangel31@gmail.com>
2021-12-16 18:58:29 +0900

    Admin오타 수정 및 shop앱 마이그레이션

--- myhomework24/shop/admin.py
+++ myhomework24/shop/admin.py
@@ -5,20 +5,20 @@ from shop.models import Category, Shop, Review, Tag
 
 
 @admin.register(Category)
-class CategoryAdmin(admin, ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Shop)
-class ShopAdmin(admin, ModelAdmin):
+class ShopAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Review)
-class ReviewAdmin(admin, ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(Tag)
-class TagAdmin(admin, ModelAdmin):
+class TagAdmin(admin.ModelAdmin):
     pass
