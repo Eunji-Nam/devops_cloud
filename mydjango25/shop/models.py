a75aa7512303efcf03346331bc6144798644153f
Eunji Nam <godangel31@gmail.com>
2021-12-17 14:32:27 +0900

    shop_list에 category_list 추가 구현

--- mydjango25/shop/models.py
+++ mydjango25/shop/models.py
@@ -58,7 +58,7 @@ class Tag(TimestampedModel):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return  self.name
+        return self.name
 
     class Meta:
         ordering = ["name"]
