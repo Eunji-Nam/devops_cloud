c486fee7a132ca5f7a1e989cffef48e253a4a8dc
Eunji Nam <godangel31@gmail.com>
2021-12-23 17:25:47 +0900

    Melon을 활용한 검색창 만들기

---
+++ myreact29/src/MelonSearch.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { Input } from 'antd';
+import Axios from 'axios';
+import jsonpAdapter from 'axios-jsonp';
+
+function MelonSearch() {
+  const [query, setQuery] = useState('');
+  const [songList, setSongList] = useState([]);
+
+  const handleChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    console.group('handleChange');
+    console.log(value);
+    console.groupEnd();
+    setQuery(value);
+  };
+  const handlePressEnter = () => {
+    console.group('handlePressEnter');
+    console.log(`검색어 ${query}(으)로 검색합니다.`);
+    console.groupEnd();
+
+    const url = 'https://www.melon.com/search/keyword/index.json';
+
+    Axios({
+      url: url,
+      adapter: jsonpAdapter,
+      callbackParamName: 'jscallback',
+      params: {
+        query: query,
+      },
+    })
+      .then((response) => {
+        // ALBUMCONTENTS, ARTISTCONTENTS도 참조 가능
+        const {
+          data: { SONGCONTENTS: searchedSongList },
+        } = response;
+        console.group('멜론 검색결과');
+        console.log(searchedSongList);
+        console.log(response);
+        console.groupEnd();
+
+        setSongList(searchedSongList);
+      })
+      .catch((error) => {
+        console.group('멜론 검색 에러');
+        console.error(error);
+        console.groupEnd();
+      });
+  };
+
+  return (
+    <div style={{ width: 300, margin: '0 auto' }}>
+      <h2>멜론 검색</h2>
+      검색어 : {query}
+      <Input
+        placeholder="검색어를 입력해주세요."
+        onChange={handleChange}
+        onPressEnter={handlePressEnter}
+      />
+      {songList.map((song) => {
+        return (
+          <div key={song.SONGID}>
+            <img src={song.ALBUMIMG} />
+            {song.SONGNAME} by {song.ARTISTNAME}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MelonSearch;
