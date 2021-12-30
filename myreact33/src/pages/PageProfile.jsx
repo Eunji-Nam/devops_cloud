91f149baf9a2b133c78a37e839be535d735033c4
Eunji Nam <godangel31@gmail.com>
2021-12-30 11:50:48 +0900

    profileList 초기값 지정하여 텍스트와 이미지만 나타나도록 표현

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react';
 
 function PageProfile() {
+  const [profileList] = useState({
+    uniqueId: 'bts-jin',
+    name: '진',
+    role: '서브보컬',
+    mbti: 'INFP',
+    instagramUrl: 'https://instagram.com/jin',
+    profileImageUrl:
+      'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
+  });
   return (
     <div>
       <h2>PageProfile</h2>
+      <img src={profileList.profileImageUrl} />
+      <h3>{profileList.name}</h3>
+      <ul>
+        <li>{profileList.role}</li>
+        <li>{profileList.mbti}</li>
+        <li>{profileList.instagramUrl}</li>
+      </ul>
     </div>
   );
 }
