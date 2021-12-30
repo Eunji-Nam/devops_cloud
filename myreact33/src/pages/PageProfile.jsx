edbd93479dacb3731ad08b09ced06777760def90
Eunji Nam <godangel31@gmail.com>
2021-12-30 14:27:01 +0900

    profileList의 상탯값이 없을 때, 나타날 메시지 등록

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -1,25 +1,34 @@
 import { useState } from 'react';
 
 function PageProfile() {
-  const [profileList] = useState({
-    uniqueId: 'bts-jin',
-    name: '진',
-    role: '서브보컬',
-    mbti: 'INFP',
-    instagramUrl: 'https://instagram.com/jin',
-    profileImageUrl:
-      'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
-  });
+  const [profileList] = useState([
+    {
+      uniqueId: 'bts-jin',
+      name: '진',
+      role: '서브보컬',
+      mbti: 'INFP',
+      instagramUrl: 'https://instagram.com/jin',
+      profileImageUrl:
+        'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
+    },
+  ]);
   return (
     <div>
       <h2>PageProfile</h2>
-      <img src={profileList.profileImageUrl} />
-      <h3>{profileList.name}</h3>
-      <ul>
-        <li>{profileList.role}</li>
-        <li>{profileList.mbti}</li>
-        <li>{profileList.instagramUrl}</li>
-      </ul>
+      {profileList.length === 0 && '등록된 프로필이 없습니다.'}
+      {profileList.map((profile) => {
+        return (
+          <div key={profile.uniqueId}>
+            <img src={profile.profileImageUrl} />
+            <h3>{profile.name}</h3>
+            <ul>
+              <li>{profile.name}</li>
+              <li>{profile.role}</li>
+              <li>{profile.instagramUrl}</li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
