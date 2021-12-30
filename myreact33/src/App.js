4fc84be37277a81838b00566d6f6aeb18a1b7d05
Eunji Nam <godangel31@gmail.com>
2021-12-30 11:41:06 +0900

    프로젝트 myreact33의 기본 레이아웃 잡기

---
+++ myreact33/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PageProfile from './pages/PageProfile';
+
+function App() {
+  return (
+    <div>
+      <PageProfile />
+    </div>
+  );
+}
+
+export default App;
---
+++ myreact33/src/pages/PageProfile.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function PageProfile() {
+  return (
+    <div>
+      <h2>PageProfile</h2>
+    </div>
+  );
+}
+
+export default PageProfile;
