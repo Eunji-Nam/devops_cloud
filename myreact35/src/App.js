4a355ac3fca82d740ab9495f7faab6f020e2dc56
Eunji Nam <godangel31@gmail.com>
2022-01-03 18:52:07 +0900

    prevValue를 이용하여 함수로 Counter 적용하기

---
+++ myreact35/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Counter from './Counter';
+import Counter3 from './Counter3';
+
+function App() {
+  return (
+    <Counter3 />
+    // <Counter initialValue={10} />
+  );
+}
+
+export default App;
