c73b474fd9783fce50f14a63e74845b29cd5d8ef
Eunji Nam <godangel31@gmail.com>
2022-01-06 01:24:18 +0900

    ReviewForm 컴포넌트 추가 후 저장할 수 있도록 구현

--- mystudy37/src/components/ReviewList.jsx
+++ mystudy37/src/components/ReviewList.jsx
@@ -1,18 +1,19 @@
 import Review from './Review';
 import './ReviewList.css';
-
 import { useState } from 'react';
+import ReviewForm from './ReviewForm';
+import useFieldValues from 'hook/useFieldValues';
 
 const INITIAL_STATE = [
   { content: '스파이더맨 1 부터 본 사람이면 재미없을 수가 없다.', star: 5 },
   {
     content:
-      '전 스파이더맨 두명이 자책했던 일들이 여기서 어느정도 구원받았다는거에 감사드립니다',
+      '전 스파이더맨 두명이 자책했던 일들이 여기서 어느정도 구원받았다는거에 감사드립니다.',
     star: 4,
   },
   {
     content:
-      '엔드게임을 뛰어넘는 영화가 죽기전에 나올까 생각했었는데.. 2년만에 나왔습니다',
+      '엔드게임을 뛰어넘는 영화가 죽기전에 나올까 생각했었는데.. 2년만에 나왔습니다.',
     star: 5,
   },
 ];
@@ -20,9 +21,27 @@ const INITIAL_STATE = [
 function ReviewList() {
   const [reviewList, setReviewList] = useState(INITIAL_STATE);
 
+  const [fieldValues, handleChange, clearFieldValues] = useFieldValues({
+    content: '',
+    star: 5,
+  });
+
+  const appendReview = () => {
+    const review = { ...fieldValues };
+
+    setReviewList((prevReviewList) => [...prevReviewList, review]);
+
+    clearFieldValues();
+  };
+
   return (
     <div className="review_list">
       <h2 className="text-xl mb-2 border-b-2 border-orange-500">Review List</h2>
+      <ReviewForm
+        fieldValues={fieldValues}
+        handleChange={handleChange}
+        handleSubmit={appendReview}
+      />
       <button className="bg-orange-200 hover:bg-orange-400 p-1 rounded text-sm cursor-point">
         New Review
       </button>
